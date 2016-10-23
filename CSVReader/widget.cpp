e0eb4ad788f5b5d6ee486c358eb59c306d461f49
Steven <banguijun@sina.com>
2016-10-23 22:23:03 +0800

    优化文件读写流程
    增加文件不存在时的行为，优化了界面组件点击回车行为。
    待增加：读写带逗号的行，显示各行数据。

--- CSVReader/widget.cpp
+++ CSVReader/widget.cpp
@@ -13,30 +13,42 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    connect(ui->addPushButton, SIGNAL(clicked(bool)), this, SLOT(addCSVItem));
-    connect(ui->savePushButton, SIGNAL(clicked(bool)), this, SLOT(saveCSV));
+    connect(ui->addPushButton, SIGNAL(clicked(bool)), this, SLOT(addCSVItem()));
+    connect(ui->savePushButton, SIGNAL(clicked(bool)), this, SLOT(saveCSV()));
+    connect(ui->lineEdit, SIGNAL(returnPressed()), this, SLOT(addCSVItem()));
 
     // read CSV file from default path
     QFile csvFile("test.csv");
     this->csvList.clear();
 
-    if(csvFile.open(QIODevice::ReadOnly)){
-        QTextStream stream(&csvFile);
-        while(!stream.atEnd()){
-            this->csvList.push_back(stream.readLine());
+    if(csvFile.exists()){
+        if(csvFile.open(QIODevice::ReadOnly)){
+            QTextStream stream(&csvFile);
+            while(!stream.atEnd()){
+                this->csvList.push_back(stream.readLine());
 
+            }
+            csvFile.close();
         }
+        ui->listWidget->addItems(this->csvList);
+        ui->listWidget->update();
+    }
+    else {
+        csvFile.open(QIODevice::WriteOnly);
+        csvFile.write("test1");
         csvFile.close();
+        QMessageBox::warning(this, tr("File not exsit!"),
+                             tr("File test.csv not exsit! Please check you default path! We have created the file."));
     }
 
-    ui->listWidget->addItems(this->csvList);
-    ui->listWidget->update();
 }
 
 void Widget::addCSVItem(){
     if(ui->lineEdit->text().length() != 0){
         this->csvList.push_back(ui->lineEdit->text());
         ui->listWidget->insertItem(ui->listWidget->count(), ui->lineEdit->text());
+        ui->lineEdit->clear();
+        ui->listWidget->update();
 
     }
     else{
